7079b1d0d6580539af1b44f895aa54784cf02308
Kevin Aguilar <11aazx1190@gmail.com>
2020-09-13 16:34:36 -0600

    methods for calculations implemented

--- ValueArray.py
+++ ValueArray.py
@@ -1,4 +1,5 @@
-from ValueSet import ValueSet
+import ValueSet
+import math
 
 class ValueArray(object):
 
@@ -14,7 +15,53 @@ class ValueArray(object):
 
     def CreateTable(self):
         self.values.sort()
+        
+        i = 0
+        while i < len(self.values):
+            aux = ValueSet(self.values.get(i),0)
+        
+            for j in self.values:
+                if aux.Mark == k:
+                    aux.incrementFreq()
+                    i+=1
+
+            self.tableFreq.add(aux)
+
+        return self.tableFreq
+
+    def CalcularModa(self):
+        modaIndex = 0
+
+        i = 0
+        while i < len(self.tableFreq):
+            if self.tableFreq[modaIndex].Freq < self.tableFreq[i].Freq:
+                modaIndex = i
+
+        return self.tableFreq[modaIndex].Mark
 
 
-    for i in values:
-        aux = ValueSet()
+    def CalcularMedia(self):
+        
+        aux = 0
+        for i in self.values:
+            aux += i
+    
+        return aux/len(values)
+
+    def CalcularMediana(self):
+        return self.values[len(self.values)/2]
+
+    def CalcularVarianza(self):
+        acMedia = 0
+        acMedia2 = 0
+
+        for i in self.values:
+            acMedia = acMedia + i
+            acMedia2 = acMedia2 + i * i
+
+        n = len(self.values)
+        
+        return (acMedia2 / (n-1) - (acMedia * acMedia) / (n * (n-1)))
+
+    def CalcularDesviacionEstandar(self):
+        return math.sqrt(self.CalcularVarianza())
