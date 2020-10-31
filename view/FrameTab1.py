7343bc7c519c56b1ff65b241a8b9e0ca31f05f3b
AguilarKevin <11aazx1190@gmail.com>
2020-10-31 15:43:45 -0600

    bug fixed in the range Textboxs

--- view/FrameTab1.py
+++ view/FrameTab1.py
@@ -1,12 +1,25 @@
-from tkinter import ttk
-
+from tkinter import ttk, END, messagebox, CENTER
 
 class FrameTab1(ttk.Frame):
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.title = ttk.Label(self)
-        self.title["text"] = ("medidas de dispersion y desviacion estandar")
-        self.title.pack(padx = 10, pady = 20)
+        self.grid_columnconfigure(0, weight = 1)
+
+        self.container = ttk.LabelFrame(self, text = 'medidas de dispersion y desviacion estandar')
+        self.container.grid(row = 0,column = 0, ipadx = 55, ipady = 35, sticky = "n, s, w, e ", padx = 5, pady = 5)
+
+        self.container_val = ttk.LabelFrame(self.container, text = 'valores')
+        self.container_val.grid(row = 0,column = 0, ipadx = 55, ipady = 35, sticky = "n, s, w", padx = 5, pady = 5)
+
+        self.container2 = ttk.LabelFrame(self, text = 'tabla de frecuencia')
+        self.container2.grid(row = 1,column = 0, ipadx = 55, ipady = 35, sticky = "n, s, e, w ", padx = 5, pady = 5)
+
+        self.freqTable = ttk.Treeview(self.container2, height = 10, columns = 2)
+        self.freqTable.heading('#0', text = "intervalo", anchor = CENTER)
+        self.freqTable.heading('#1', text = "Frequencia", anchor = CENTER)
+        self.freqTable.grid(row = 0,column = 0, ipadx = 55, ipady = 35, sticky = "N, S", padx = 5, pady =5)
+
+
 
--- view/FrameTab2.py
+++ view/FrameTab2.py
@@ -1,76 +1,78 @@
-from tkinter import *
-from tkinter import messagebox
+from tkinter import ttk, END, messagebox, IntVar, Text
 import calc.binomial as binom
 
-class FrameTab2(Frame):
+class FrameTab2(ttk.Frame):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.container = LabelFrame(self, text = 'Distribucion binomial')
-        self.container.grid(row = 0, ipadx = 55, ipady = 35, sticky = "N, S", padx = 7, pady = 20)
 
-        self.n_label = Label(self.container)
+        self.grid_rowconfigure(0, weight = 1)
+        
+        self.container = ttk.LabelFrame(self, text = 'Distribucion binomial')
+        self.container.grid(row = 0, column = 0, ipadx = 30, ipady = 35, sticky = "N, S", padx = 7, pady = 20)
+
+        self.n_label = ttk.Label(self.container)
         self.n_label["text"] = ("n:")
-        self.n_label.grid(row = 0, column = 0, padx = 8, pady = 16)
+        self.n_label.grid(row = 0, column = 0, padx = 8, pady = 8)
 
-        self.input_n = Entry(self.container, width = 7)
+        self.input_n = ttk.Entry(self.container, width = 7)
         self.input_n.grid(row = 0, column = 1)
 
-        self.p_label = Label(self.container)
+        self.p_label = ttk.Label(self.container)
         self.p_label["text"] = ("p:")
-        self.p_label.grid(row = 1, column = 0, padx = 8, pady = 16)
+        self.p_label.grid(row = 1, column = 0, padx = 8, pady = 8)
         
-        self.input_p = Entry(self.container, width = 7)
+        self.input_p = ttk.Entry(self.container, width = 7)
         self.input_p.grid(row = 1, column = 1)
 
         self.selection = IntVar()
 
-        self.R1 = Radiobutton(self.container, text="P( X >=", variable=self.selection, value=1, command = self.enable_btnCalc)
-        self.R1.grid(row = 3, column = 0)
+        self.R1 = ttk.Radiobutton(self.container, text="P( X >=", variable=self.selection, value=1, command = self.switch)
+        self.R1.grid(row = 3, column = 0, padx = 8, pady = 8)
 
-        self.input_r1 = Entry(self.container, width = 7)
-        self.input_r1.grid(row = 3, column = 1)
+        self.input_r1 = ttk.Entry(self.container, width = 7)
+        self.input_r1.grid(row = 3, column = 1,)
 
-        self.r1_label = Label(self.container)
+        self.r1_label = ttk.Label(self.container)
         self.r1_label["text"] = (")")
         self.r1_label.grid(row = 3, column = 2)
 
-        self.R2 = Radiobutton(self.container, text="P( X <=", variable=self.selection, value=2, command = self.enable_btnCalc)
-        self.R2.grid(row = 4, column = 0)
+        self.R2 = ttk.Radiobutton(self.container, text="P( X <=", variable=self.selection, value=2, command = self.switch)
+        self.R2.grid(row = 4, column = 0, padx = 8, pady = 8)
 
-        self.input_r2 = Entry(self.container, width = 7)
+        self.input_r2 = ttk.Entry(self.container, width = 7)
         self.input_r2.grid(row = 4, column = 1)
 
-        self.r2_label = Label(self.container)
+        self.r2_label = ttk.Label(self.container)
         self.r2_label["text"] = (")")
         self.r2_label.grid(row = 4, column = 2)
 
-        self.R3 = Radiobutton(self.container, text="P( X =", variable=self.selection, value=3, command = self.enable_btnCalc)
-        self.R3.grid(row = 5, column = 0)
+        self.R3 = ttk.Radiobutton(self.container, text="P( X =", variable=self.selection, value=3, command = self.switch)
+        self.R3.grid(row = 5, column = 0, padx = 8, pady = 8)
 
-        self.input_r3 = Entry(self.container, width = 7)
+        self.input_r3 = ttk.Entry(self.container, width = 7)
         self.input_r3.grid(row = 5, column = 1)
 
-        self.r3_label = Label(self.container)
+        self.r3_label = ttk.Label(self.container)
         self.r3_label["text"] = (")")
         self.r3_label.grid(row = 5, column = 2)
 
-        self.calc_btn = Button(self.container, text = "calcular", command = self.calc)
+        self.calc_btn = ttk.Button(self.container, text = "calcular", command = self.calc)
         self.calc_btn["state"] = "disabled"
-        self.calc_btn.grid(row = 7, column = 1, padx = 8, pady = 16)
+        self.calc_btn.grid(row = 7, column = 1, padx = 5, pady = 16)
 
-        self.clear_btn = Button(self.container, text = "limpiar", command = self.clear_Entries)
-        self.clear_btn.grid(row = 7, column = 2, padx = 8, pady = 16 )
+        self.clear_btn = ttk.Button(self.container, text = "limpiar", command = self.clear_Entries)
+        self.clear_btn.grid(row = 7, column = 2, padx = 5, pady = 16 )
 
         #answer text area
-        self.container2 = LabelFrame(self, text = 'Respuestas')
-        self.container2.grid(row = 0, column = 1)
+        self.container2 = ttk.LabelFrame(self, text = 'Respuestas')
+        self.container2.grid(row = 0, column = 1, ipadx = 55, ipady = 35, sticky = "N, S", padx = 7, pady = 20)
 
         self.answerTextArea = Text(self.container2, width = 45)
         self.answerTextArea.pack(side = 'left')
         #grid(column = 0, row = 0, columnspan = 1, padx = 16, pady = 20)
         
-        self.scrollbar = Scrollbar(self.container2)
+        self.scrollbar = ttk.Scrollbar(self.container2)
         self.scrollbar.pack(side = 'right', fill = "y", expand = False)
 
         self.answerTextArea.config( yscrollcommand = self.scrollbar.set )
@@ -108,11 +110,25 @@ class FrameTab2(Frame):
         self.input_n.delete(0,END)
         self.input_p.delete(0,END)
         self.input_r1delete(0,END)
-        self.input_r3delete(0,END)
+        self.input_r2delete(0,END)
         self.input_r3delete(0,END)
 
+    def switch(self):
+        self.enable_btnCalc()
+        
+        if self.selection == 1:
+            self.input_r2delete(0,END)
+            self.input_r3delete(0,END)
+        elif self.selection == 2:
+            self.input_r1delete(0,END)
+            self.input_r3delete(0,END)
+        elif self.selection == 3:
+            self.input_r1delete(0,END)
+            self.input_r2delete(0,END)
+
     def enable_btnCalc(self):
-        self.calc_btn["state"] = "normal"
+        if self.calc_btn["state"] == "disabled":
+            self.calc_btn["state"] = "normal"
 
     def rangeIsEmpty(self, i):
         if i == 1:
@@ -120,20 +136,20 @@ class FrameTab2(Frame):
         elif i == 2: 
             return len(self.input_r2.get()) == 0
         elif i == 3:
-            return len(self.input_r1.get()) == 0
+            return len(self.input_r3.get()) == 0
     
 
     def validateEntries(self):
         if len(self.input_n.get()) > 0 and len(self.input_p.get()) > 0:
             if self.rangeIsEmpty(self.selection.get()):
-                messagebox.showinfo(title = "Error", message = "ingrese el rango") 
+                ttk.messagebox.showinfo(title = "Error", message = "ingrese el rango") 
             else:
                 if float(self.input_p.get()) > 1:
-                    messagebox.showinfo(title = "Error", message = "p no puede ser mayor a 1")
+                    ttk.messagebox.showinfo(title = "Error", message = "p no puede ser mayor a 1")
                 else:
                     return True
         else:
-            messagebox.showinfo(title = "Error", message = "rellene todos los campos")
+            ttk.messagebox.showinfo(title = "Error", message = "rellene todos los campos")
         
         return False
 
