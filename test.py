92e312c485ae74cb63c66fc6d10ece258b3b8a6b
AguilarKevin <11aazx1190@gmail.com>
2020-11-23 08:25:54 -0600

    normal distribution table implemented

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
-from tkinter import ttk  # import tkinter.ttk as ttk for Python 3
-s=ttk.Style()
-print(s.theme_names())
+from scipy.integrate import quad
+import numpy as np
+import matplotlib.pyplot as plt
+
+def standardize(x, mean, standartDeviation):
+        return ((x - mean) / standartDeviation)
+
+def normalProbabilityDensity (x):
+    constante = 1.0 / np.sqrt (2 * np.pi)
+    return (constante * np.exp ((- x ** 2) / 2.0))
+
+
+z = standardize(185, 170, 12)
+
+print(quad (normalProbabilityDensity, np.NINF , z))
